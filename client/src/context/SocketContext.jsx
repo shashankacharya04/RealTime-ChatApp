58cc6cd3b6d46ac9b251005271dd2cc4079ff3ca
shashank <achryashashank@gmail.com>
2024-04-06 11:30:46 +0530

    updated socket url

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -13,7 +13,7 @@ export const SocketContextProvider = ({ children }) => {
   const { authUser } = useAuthContext();
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chat-app-prod-8esa.onrender.com/", {
         query: { userId: authUser._id },
       });
       socket.on("getOnlineUser", (users) => {
