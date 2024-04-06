261c2227e7e416bd8fd79606013cde6d07c182de
shashank <achryashashank@gmail.com>
2024-04-06 10:22:01 +0530

    deployment

--- back-end/index.js
+++ back-end/index.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
@@ -14,12 +15,20 @@ dotenv.config(); // used to use ports from .env file
 
 
 const PORT = process.env.PORT || 3000
+const __dirname = path.resolve();
 
 app.use(cookieParser())
 app.use(express.json());   //parsing requests with json payload  req.body
+
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/user",userRoutes);
+
+app.use(express.static(path.join(__dirname,"/client/dist")));
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"client","dist","index.html"));
+})
 
 server.listen(PORT,()=>{
     connectToMongoDb();
