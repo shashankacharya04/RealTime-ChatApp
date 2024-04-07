a65f98b296d8d8b05d13c3a73721ab46a8f952e8
shashank <achryashashank@gmail.com>
2024-04-07 10:41:56 +0530

    new branch

--- client/src/components/sidebar/SideBar.jsx
+++ client/src/components/sidebar/SideBar.jsx
@@ -3,10 +3,12 @@ import SearchInput from "./SearchInput";
 import Conversation from "./Conversation";
 import LogoutBtn from "./LogoutBtn";
 import Conversations from "./Conversations";
+import AddRoom from "./AddRoom";
 const SideBar = () => {
   return (
     <div className="border-r border-slate-800 p-3 flex flex-col">
       <SearchInput />
+      <AddRoom />
       <div className="divider px-3"></div>
       <Conversations />
       <LogoutBtn />
