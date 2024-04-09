fbfd0c1a2ac77bbd001362b0d6c082eaa98d0fe7
shashank <achryashashank@gmail.com>
2024-04-09 16:53:22 +0530

    test2

--- client/src/components/sidebar/SideBar.jsx
+++ client/src/components/sidebar/SideBar.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import SearchInput from "./SearchInput";
-import Conversation from "./Conversation";
 import LogoutBtn from "./LogoutBtn";
 import Conversations from "./Conversations";
-import AddRoom from "./AddRoom";
 const SideBar = () => {
   return (
     <div className="border-r border-slate-800 p-3 flex flex-col">
       <SearchInput />
-      <AddRoom />
       <div className="divider px-3"></div>
       <Conversations />
       <LogoutBtn />
