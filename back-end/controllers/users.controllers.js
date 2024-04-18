e3a1bf36362a63c7e4d91f9eb25081e2b2af7346
shashank <achryashashank@gmail.com>
2024-04-18 13:04:26 +0530

    mistake

--- back-end/controllers/users.controllers.js
+++ back-end/controllers/users.controllers.js
@@ -15,31 +15,35 @@ export const getUsersForSideBar = async(req,res)=>{
     }
 
 }
-export const updatedUser=async(req,res)=>{
-    try {
-        const {id} = req.params;
-const {fullName,password,confirmPassword,profilePic} = req.body;
-const user = await User.findById(id);
-console.log("user is",user)
-// if(user !==null) {
-//     return res.status(404).json({
-//         error:"user not found"
+// export const updatedUser=async(req,res)=>{
+//     res.json({
+//         message:"helo there"
 //     })
+// //     try {
+// //         const {id} = req.params;
+// // const {fullName,password,confirmPassword,profilePic} = req.body;
+// // const user = await User.findById(id);
+// // console.log("user is",user)
+// // // if(user !==null) {
+// // //     return res.status(404).json({
+// // //         error:"user not found"
+// // //     })
+// // // }
+// // console.log("hello thrre");
+// // if(password !== confirmPassword){
+// //     return res.status(400).json({
+// //         error:"password does not match"
+// //     })
+// // }
+// // const updateuser = await User.findOneAndUpdate({_id:id},{
+// //     $set:{
+// //         fullName,
+// //         password
+// //     },
+// //      upsert:false
+// // })
+// // console.log("user",updateuser)
+// //     } catch (error) {
+// //         console.log("error is",error)
+// //     }
 // }
-if(password !== confirmPassword){
-    return res.status(400).json({
-        error:"password does not match"
-    })
-}
-const updateuser = await User.findOneAndUpdate({_id:id},{
-    $set:{
-        fullName,
-        password
-    },
-     upsert:false
-})
-console.log("user",updateuser)
-    } catch (error) {
-        console.log("error is",error)
-    }
-}
